9310a8d56645092050e79611a9ce5311a4951291
Aleksandr <sanyacool2010@mail.ru>
2017-05-07 00:21:49 +0800

    start 20datofcode

---
+++ index.js
@@ -0,0 +1,28 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/in', function(req, res){
+  res.sendFile(__dirname + '/in.html');
+});
+
+app.get('/out', function(req, res){
+  res.sendFile(__dirname + '/out.html');
+});
+ 
+io.on('connection', function(socket){
+  var name = 'U' + (socket.id).toString().substr(1,4);
+  socket.emit('user connected', name);
+  socket.on('button clicked', function(value){
+    io.emit('button clicked', name, value);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
